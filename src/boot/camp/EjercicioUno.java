08ad732084ed6c51857f0d440b2f6eb81f042782
SBolivarLoL <mikelmd@criptext.com>
2022-10-24 00:08:42 -0500

    Ejercicio 1

---
+++ src/boot/camp/EjercicioUno.java
@@ -0,0 +1,26 @@
+package boot.camp;
+
+public class EjercicioUno {
+
+    public static void main(String[] args) {
+
+        int integer = 123;
+        double doble = 12.34d;
+        float flotante = 123.4f;
+        long largo = 1234567890;
+        String cadena = "candy candy";
+        char caracter = 'a';
+        boolean bulean = false;
+
+
+        System.out.println("Entero: " + integer);
+        System.out.println("Doble: " + doble);
+        System.out.println("Flotante: " + flotante);
+        System.out.println("Largo: " + largo);
+        System.out.println("String: " + cadena);
+        System.out.println("Letra: " + caracter);
+        System.out.println("Buleano: " + bulean);
+        System.out.println("Entero: " + integer);
+
+    }
+}
