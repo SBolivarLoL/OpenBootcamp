0b76a759e2b33ecf9fa929039bcd9b1fd3cbe041
SBolivarLoL <mikelmd@criptext.com>
2022-10-26 17:25:27 -0500

    Ejercicio 3

--- src/boot/camp/EjercicioDos.java
+++ src/boot/camp/EjercicioDos.java
@@ -3,7 +3,6 @@ package boot.camp;
 public class EjercicioDos {
 
     public static void main(String[] args) {
-
     }
 
     public static double agregarIVA(int precio) {
---
+++ src/boot/camp/EjercicioTres.java
@@ -0,0 +1,19 @@
+package boot.camp;
+
+public class EjercicioTres {
+
+    public static void main(String[] args) {
+
+        String[] testos = {"la", "mamá", "de", "la", "mamá"};
+        String result = concatenar(testos);
+        System.out.println(result);
+    }
+
+    static String concatenar(String[] textos){
+        String result = "";
+        for (String texto : textos) {
+            result += " " + texto;
+        }
+        return result;
+    }
+}
