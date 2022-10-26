649d63e47ed35fd59cfbcf7e09b6e8daa10ecb8f
SBolivarLoL <mikelmd@criptext.com>
2022-10-26 13:21:50 -0500

    Ejercicio 2

---
+++ src/boot/camp/EjercicioDos.java
@@ -0,0 +1,13 @@
+package boot.camp;
+
+public class EjercicioDos {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double agregarIVA(int precio) {
+        double iva = precio * 0.21;
+        return precio + iva;
+    }
+}
